b3787f0fae060d69d78a0d75ca0557a939b2ad12
Muhammad Maaz Uddin <maaznadeem246@gmail.com>
2021-06-17 08:19:06 -0700

    front div animtion

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
   return (
     <div className="App">
       <MainComp >
-        <MainFrontComp />
+
       </MainComp>
+      <MainFrontComp />
       
     </div>
   );
--- src/components/mainComp.js
+++ src/components/mainComp.js
@@ -1,22 +1,47 @@
 import React from "react"
+import { useSpring, animated } from "react-spring"
 import Styled from "styled-components"
 
 
 const BackDiv = Styled.div`
-    background-color:#00ff0314;
-    position: absolute;
-    top: 0px;
-    right: 0px;
-    bottom: 0px;
-    left: 0px;
-    border:12px solid white;
+background-color:white;
+    height:100vh;
+    :before{
+        content:" ";
+        position:absolute;
+        top:12px;
+        right:12px;
+        left:12px;
+        bottom:12px;
+
+        background-color:#00ff0314;
+    }
 `
+const styles = {
+    mainD:{
+        postition:"relative",
+    }
+}
 
 function MainComp({children}){
+
+    const stylesAnim = useSpring({
+        config:{duration:500},
+        delay:300,
+        to:[
+            { opacity: 0.6, transform:'scale(1,1)'},
+            { opacity: 1,},
+          ],
+        from: { opacity: 0, transform:'scale(0.6,0.6)'},
+      })
+      
     return (
+        <animated.div  className={styles.mainD} style={stylesAnim}>
+         
         <BackDiv>
             {children}
         </BackDiv>
+        </animated.div>
     )
 }
 
